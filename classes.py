625419876dc8c498f1a09939ca18bf9d938872ed
Lathe Ward <lward21@basecampcodingacademy.org>
2020-11-09 13:55:02 -0600

    class numerical is good, finishing init's for other classes

--- classes.py
+++ classes.py
@@ -2,24 +2,30 @@ class true_false:
     def __init__(self, prompt, response, answer):
         self.prompt = prompt
         self.response = response
-
+        self.answer = answer
 
 class multiple_choice:
-    def __init__(self, prompt, qone, qtwo, qthree, qfour, qfive):
+    def __init__(self, prompt, questions, answers):
         self.prompt = prompt
+        self.questions = questions
+        self.answers = answers
 
 
 class numerical:
-    def __init__(self, prompt, num, answr):
+    def __init__(self, prompt, num, answer):
         self.prompt = prompt
         self.num = num
 
-    def num_answer(self, num, answr):
-        return num == answr
+    def num_answer(self, answr):
+        return self == answr
 
 
 class fill_blank:
-    def __init__(self, prompt, word, answr):
+    def __init__(self, prompt, word, answer):
         self.prompt = prompt
         self.word = word
+        self.answer = answer
+
+    def fill_blank_answer(self,)
+
 
