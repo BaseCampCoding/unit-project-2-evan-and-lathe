1e89cc05d4c881fd0e4267f06e15be3a4dca08dc
Lathe Ward <lward21@basecampcodingacademy.org>
2020-11-09 13:58:31 -0600

    defined answer functions

--- classes.py
+++ classes.py
@@ -4,11 +4,18 @@ class true_false:
         self.response = response
         self.answer = answer
 
+    def true_false_answers(self, answer):
+        return self == answer
+
+
 class multiple_choice:
     def __init__(self, prompt, questions, answers):
         self.prompt = prompt
         self.questions = questions
         self.answers = answers
+
+    def multiple_choice_answers(self, answers):
+        return self == answers
 
 
 class numerical:
@@ -16,8 +23,8 @@ class numerical:
         self.prompt = prompt
         self.num = num
 
-    def num_answer(self, answr):
-        return self == answr
+    def num_answer(self, answer):
+        return self == answer
 
 
 class fill_blank:
@@ -26,6 +33,5 @@ class fill_blank:
         self.word = word
         self.answer = answer
 
-    def fill_blank_answer(self,)
-
-
+    def fill_blank_answer(self, answer):
+        return self == answer
