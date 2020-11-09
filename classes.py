064f1cfb660db7f5c99e7f37ac21e4dd024b8f57
Lathe Ward <lward21@basecampcodingacademy.org>
2020-11-09 13:03:19 -0600

    have starts on classes for questions

---
+++ classes.py
@@ -0,0 +1,22 @@
+class true_false:
+    def __init__(self, prompt, response):
+        self.prompt = prompt
+        self.response = response
+
+
+
+class multiple_choice:
+    def __init__(self, qone, qtwo):
+
+
+class numerical:
+    def __init__(self, num):
+        self.num = num
+
+
+class fill_blank:
+    def __init__(self, word):
+       self.word = word
+
+        
+
