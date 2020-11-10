afee782c56f5fdd945e3d9d04e6a75b3b31d1b23
Lathe Ward <lward21@basecampcodingacademy.org>
2020-11-10 08:33:55 -0600

    Added 2 inputs for later

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print("WELCOME TO QUIZZER")
 
 
 def funct1():
-    while True: 
+    while True:
         name = input("What would you like to name your quiz?: ")
         correct_input = input("Your quiz is called " + name + " is this correct?: ")
         if correct_input.upper() == "Y":
@@ -10,5 +10,11 @@ def funct1():
             break
         elif correct_input.upper() == "N":
             funct1()
+
+
 funct1()
 print("How are you?")
+
+how_many_tf = input("How many True/False questions would you like")
+
+how_many_num = input("How many numerical answer questions would you like?")
