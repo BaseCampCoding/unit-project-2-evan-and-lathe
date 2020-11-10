dc807b4d83771205d2dbbe219d358100bc521a67
Evan Sanders <esanders21@basecampcodingacademy.org>
2020-11-10 09:10:16 -0600

    Yes

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 print("WELCOME TO QUIZZER")
 
 
-def funct1():
-    while True:
-        name = input("What would you like to name your quiz?: ")
-        correct_input = input("Your quiz is called " + name + " is this correct?: ")
-        if correct_input.upper() == "Y":
-            print("ok")
-            break
-        elif correct_input.upper() == "N":
-            funct1()
 
-
-funct1()
+while True:
+    name = input("What would you like to name your quiz?: ")
+    correct_input = input("Your quiz is called " + name + " is this correct?: ")
+    if correct_input.upper() == "YES":
+        print("ok")
+        break
+    elif correct_input.upper() == "NO":
+        continue
+            
 print("How are you?")
 
 how_many_tf = input("How many True/False questions would you like")
 
 how_many_num = input("How many numerical answer questions would you like?")
+
+how_many_multiple = input("How many multiple choice questions would you like?")
+
+how_many_fib = input("How many fill in the blank questions would you like?")
