e59ba70c0ebc1e84bffbfe84e964fa7cf43a64a7
Lathe Ward <lward21@basecampcodingacademy.org>
2020-11-11 13:48:39 -0600

    finalized printing, changed fb items into list of lists

--- addtomain.py
+++ addtomain.py
@@ -14,51 +14,50 @@ for i in range(how_many_fb):
     # response_fb = input( )
     answer_fb = input("What is the correct answer? ")
     fb_questions = classes.fill_blank(prompt_fb, None, answer_fb)
-    all_fb_questions.append(fb_questions.prompt)
-    all_fb_answers.append(fb_questions.answer)
-    object_fb_questions.append(fb_questions) 
+    all_fb_questions.append([fb_questions.prompt])
+    all_fb_answers.append([fb_questions.answer])
+    object_fb_questions.append([fb_questions])
 
 # Multiple choice questions
 
-# how_many_mc = input("How many mc questions would you like? ")
-# how_many_mc = int(how_many_mc)
-# all_mc_questions = []
-# all_mc_choices = []
-# all_mc_answers = []
-# object_mc_questions = []
+how_many_mc = input("How many mc questions would you like? ")
+how_many_mc = int(how_many_mc)
+all_mc_questions = []
+all_mc_choices = []
+all_mc_answers = []
+object_mc_questions = []
 
-# for i in range(how_many_mc):
-#     prompt_mc = input("What would you like the question to say? ")
-#     # response_mc = input( )
-#     mc_choices = input("What are the answer choices (Including the correct answer): ")
-#     answer_mc = input("What is the correct answer: ")
-#     mc_questions = classes.multiple_choice(prompt_mc, None, mc_choices, answer_mc)
-#     all_mc_choices.append([mc_questions.choices])
-#     all_mc_answers.append([mc_questions.answer])
-#     all_mc_questions.append([mc_questions.prompt])
-#     object_mc_questions.append(mc_questions)
+for i in range(how_many_mc):
+    prompt_mc = input("What would you like the question to say? ")
+    # response_mc = input( )
+    mc_choices = input("What are the answer choices (Including the correct answer): ")
+    answer_mc = input("What is the correct answer: ")
+    mc_questions = classes.multiple_choice(prompt_mc, None, mc_choices, answer_mc)
+    all_mc_choices.append([mc_questions.choices])
+    all_mc_answers.append([mc_questions.answer])
+    all_mc_questions.append([mc_questions.prompt])
+    object_mc_questions.append(mc_questions)
 
 # # # presenting quiz
 print("Here is your quiz. \nAnswer the questions in chronological order ")
 
 
-# for i in all_fb_answers:
 for index, i in enumerate(all_fb_answers):
-    print()
+    print(all_fb_questions)
     fb_user_input = input("\nAnswer:")
-    if fb_user_input == i:
+    if fb_user_input in i:
         print("That's right")
         print("--------")
     else:
         print("Incorrect")
 
-# print("\nSee the list below the question for your answer choices")
-# for index, q in enumerate(all_mc_answers):
-#     pprint.pprint(all_mc_questions)
-#     pprint.pprint(all_mc_choices)
-#     mc_user_input = input("\nAnswer:")
-#     if mc_user_input in q:
-#         print("That's right")
-#         print("--------")
-#     else:
-#         print("Incorrect")
+print("\nSee the list below the question for your answer choices")
+for index, q in enumerate(all_mc_answers):
+    pprint.pprint(all_mc_questions)
+    pprint.pprint(all_mc_choices)
+    mc_user_input = input("\nAnswer:")
+    if mc_user_input in q:
+        print("That's right")
+        print("--------")
+    else:
+        print("Incorrect")
